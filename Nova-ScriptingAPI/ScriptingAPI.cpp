1d1e1d725361312f2bacee527820fa604189b04d
Ang-Zhi-Wei <56944484+Ang-Zhi-Wei@users.noreply.github.com>
2025-08-08 19:07:14 +0800

    .Net Hosting with CoreCLR

---
+++ Nova-ScriptingAPI/ScriptingAPI.cpp
@@ -0,0 +1,8 @@
+#include "ScriptingAPI.h"
+namespace ScriptingAPI {
+	void Interface::TestPrint()
+	{
+		System::Console::WriteLine("This should print on the console if it works");
+	}
+}
+
---
+++ Nova-ScriptingAPI/ScriptingAPI.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace ScriptingAPI {
+	public ref class Interface
+	{
+	public:
+		static void TestPrint();
+	};
+}
+
+
