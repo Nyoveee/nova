580c4990bd9387b62e53f4cc264928ff088d87f5
jjooel28 <joelteo1188@gmail.com>
2025-09-18 15:53:37 +0800

    bruh forgot to untick before pushing previous

--- Nova-Engine/src/Graphics/renderer.cpp
+++ Nova-Engine/src/Graphics/renderer.cpp
@@ -145,28 +145,6 @@ Renderer::Renderer(Engine& engine, int gameWidth, int gameHeight) :
 	// associate vertex attribute 0 with binding index 1.
 	glVertexArrayAttribBinding(debugPhysicsVAO, 0, debugBindingIndex);
 
-	// ======================================================
-	// Fullscreen Quad VAO configuration for post-processing
-	// ======================================================
-	const float quadVertices[] = {
-		// positions   // texCoords
-		-1.0f,  1.0f,  0.0f, 1.0f,
-		-1.0f, -1.0f,  0.0f, 0.0f,
-		 1.0f,  1.0f,  1.0f, 1.0f,
-		 1.0f, -1.0f,  1.0f, 0.0f,
-	};
-
-	glGenVertexArrays(1, &quadVAO);
-	glGenBuffers(1, &quadVBO);
-
-	glBindVertexArray(quadVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 }
 
 Renderer::~Renderer() {
