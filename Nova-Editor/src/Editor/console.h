2fee927d6ce61092b7a9b09fce6d7422d16f5a69
nivethavarsni <nivethavarsni17@gmail.com>
2025-11-23 20:41:14 +0800

    Debug not displayed by default
    Set debug to false so it doesn't display by default now if we want we can on it.

--- Nova-Editor/src/Editor/console.h
+++ Nova-Editor/src/Editor/console.h
@@ -6,7 +6,7 @@ class Console {
 private:
     bool autoScroll = false;
     bool showInfo = true;
-    bool showDebug = true;
+    bool showDebug = false;
     bool showWarnings = true;
     bool showErrors = true;
 
