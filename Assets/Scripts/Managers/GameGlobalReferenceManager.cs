34053fd3492020c7490d0e427894864625979843
Ang-Zhi-Wei <56944484+Ang-Zhi-Wei@users.noreply.github.com>
2025-11-28 15:06:42 +0800

    [WIP] Tutorial

--- Assets/Scripts/Managers/GameGlobalReferenceManager.cs
+++ Assets/Scripts/Managers/GameGlobalReferenceManager.cs
@@ -4,10 +4,13 @@
 class GameGlobalReferenceManager : Script
 {
     public GameObject[] vantagePoints;
+    [SerializableField]
+    private MeshRenderer_ levelRenderer;
     // This function is first invoked when game starts.
     protected override void init()
     {
         vantagePoints = GameObject.FindGameObjectsWithTag("Vantage Point");
+        levelRenderer.setMaterialFloat(6, "intensity", 0.5f);
     }
 
     // This function is invoked every fixed update.
---
+++ Assets/Scripts/Quests/Tutorial/InteractTutorialQuest.cs
@@ -0,0 +1,43 @@
+// Make sure the class name matches the filepath, without space!!.
+// If you want to change class name, change the asset name in the editor!
+// Editor will automatically rename and recompile this file.
+using ScriptingAPI;
+
+class InteractTutorialQuest : Quest
+{
+    [SerializableField]
+    private GameObject? interactable;
+    [SerializableField]
+    private GameObject? player;
+    [SerializableField]
+    private MeshRenderer_? renderer;
+    [SerializableField]
+    private Transform_? cameraTransform;
+    // This function is first invoked when game starts.
+    public override void OnEnter() {
+        MapKey(Key.E, CheckInteraction);
+    }
+
+    public override void OnSuccess(){
+        renderer.setMaterialFloat(6, "intensity", 9f);
+    }
+    public override void OnFail(Transform_ playerTransform){
+        if (playerTransform != null && playerCheckpoint != null)
+            playerTransform.position = playerCheckpoint.position;
+    }
+
+    public override void UpdateQuest() { }
+    private bool IsLookingAtGenerator()
+    {
+        RayCastResult? result = PhysicsAPI.Raycast(cameraTransform.position, cameraTransform.front, 1000f, player);
+        if (result == null)
+            return false;
+        return interactable == new GameObject(result.Value.entity);
+    }
+    private void CheckInteraction()
+    {
+        if (IsLookingAtGenerator())
+            SetQuestState(QuestState.Success);
+    }
+
+}
