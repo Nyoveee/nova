5a7eb378d7d15488ac491f9cc8c3ce44816e707e
Nyoveee <nyove@hotmail.com>
2025-12-21 17:35:39 +0800

    Fixed system resource compilation and outdated names.

--- Nova-Framework/src/Resource/systemResource.cpp
+++ Nova-Framework/src/Resource/systemResource.cpp
@@ -11,19 +11,19 @@
 const std::unordered_map<ResourceID, ResourceFilePath> AssetIO::systemModelResources {
 	{	CUBE_MODEL_ID,					SystemFilePath(Model, "Cube")		},
 	{	SPHERE_MODEL_ID,				SystemFilePath(Model, "Sphere")		},
-	{	CAPSULE_MODEL_ID,				SystemFilePath(Model, "Capsule") }
+	{	CAPSULE_MODEL_ID,				SystemFilePath(Model, "Capsule")	}
 };
 
 // =============================== Shader =============================== 
 const std::unordered_map<ResourceID, ResourceFilePath> AssetIO::systemShaderResources{
-	{	DEFAULT_PBR_SHADER_ID,			SystemFilePath(CustomShader, "DefaultPBR")		},
-	{	DEFAULT_COLOR_SHADER_ID,		SystemFilePath(CustomShader, "DefaultColor")	}
+	{	DEFAULT_PBR_SHADER_ID,			SystemFilePath(CustomShader, "[Default] PBR Shader")	},
+	{	DEFAULT_COLOR_SHADER_ID,		SystemFilePath(CustomShader, "[Default] Color Shader")	}
 };
 
 // =============================== Material =============================== 
 const std::unordered_map<ResourceID, ResourceFilePath> AssetIO::systemMaterialResources{
-	{	DEFAULT_PBR_MATERIAL_ID,		SystemFilePath(Material, "DefaultPBRMaterial")		},
-	{	DEFAULT_COLOR_MATERIAL_ID,		SystemFilePath(Material, "DefaultColorMaterial")	}
+	{	DEFAULT_PBR_MATERIAL_ID,		SystemFilePath(Material, "[Default] PBR Material")		},
+	{	DEFAULT_COLOR_MATERIAL_ID,		SystemFilePath(Material, "[Default] Color Material")	}
 };
 
 // =============================== Texture =============================== 
