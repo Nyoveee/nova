2ffb38f89e912f7da0c9d36b09ed78d0fc18534e
snyxxel <67882151+snyxxel@users.noreply.github.com>
2026-01-18 02:18:03 +0800

    BGM Script Updated*
    Didn't save with last commit

--- Assets/Scripts/Managers/GameBGMManager.cs
+++ Assets/Scripts/Managers/GameBGMManager.cs
@@ -11,6 +11,7 @@ class GameBGMManager : Script
         NonCombat,
         Transition,
         Combat,
+        Level1Complete
     }
     private BGMState bgmState = BGMState.NonCombat;
     private Dictionary<BGMState, CurrentState> updateState = new Dictionary<BGMState, CurrentState>();
@@ -23,14 +24,15 @@ class GameBGMManager : Script
     private float transitionTime;
     [SerializableField]
     private float bufferTime;
- 
+
     // This function is first invoked when game starts.
     protected override void init()
     {
-        AudioAPI.PlayBGM(gameObject, "BGM_Vestigial_Nu_BGM-1st-Part_Loop_140bpm");
+        AudioAPI.PlayBGM(gameObject, "BGM_Vestigal_Perc-Action_BGM_Loop_140bpm");
         updateState.Add(BGMState.NonCombat, NonCombatState);
         updateState.Add(BGMState.Transition, TransitionState);
         updateState.Add(BGMState.Combat, CombatState);
+        updateState.Add(BGMState.Level1Complete, Level1CompleteState);
         currentBufferTime = bufferTime;
     }
 
@@ -45,7 +47,7 @@ class GameBGMManager : Script
         GameObject[] pods = GameObject.FindGameObjectsWithTag("Pod");
         if (pods.Length > 0)
             return true;
-        foreach(GameObject gameObject in gameObjects)
+        foreach (GameObject gameObject in gameObjects)
         {
             Enemy? enemy = gameObject.getScript<Enemy>();
             if (enemy == null)
@@ -60,6 +62,7 @@ class GameBGMManager : Script
    **********************************************************************/
     private void NonCombatState()
     {
+        /*
         if (IsInCombat())
         {
             currentBufferTime -= Time.V_DeltaTime();
@@ -71,13 +74,17 @@ class GameBGMManager : Script
             }
         }
         else
-            currentBufferTime = bufferTime;
+            currentBufferTime = bufferTime; 
+        */
     }
     private void TransitionState()
     {
+        /*
         currentTransitionTimer += Time.V_DeltaTime();
-        if(currentTransitionTimer >= transitionTime){
-            if (IsInCombat()){
+        if (currentTransitionTimer >= transitionTime)
+        {
+            if (IsInCombat())
+            {
                 bgmState = BGMState.Combat;
                 AudioAPI.PlayBGM(gameObject, "BGM_Vestigial_Nu_BGM-2nd-Part_DubStep_Loop_140bpm");
                 currentBufferTime = bufferTime;
@@ -85,10 +92,12 @@ class GameBGMManager : Script
             }
             bgmState = BGMState.NonCombat;
             AudioAPI.PlayBGM(gameObject, "BGM_Vestigial_Nu_BGM-1st-Part_Loop_140bpm");
-        }
+        } 
+        */
     }
     private void CombatState()
     {
+        /*
         if (!IsInCombat())
         {
             currentBufferTime -= Time.V_DeltaTime();
@@ -96,6 +105,23 @@ class GameBGMManager : Script
             {
                 bgmState = BGMState.Transition;
                 AudioAPI.PlayBGM(gameObject, "BGM_Vestigial_Nu_BGM-Transition_Linear_140bpm");
+                currentTransitionTimer = 0f;
+                return;
+            }
+        }
+        else
+            currentBufferTime = bufferTime;
+        */
+    }
+    private void Level1CompleteState()
+    {
+        if (!IsInCombat())
+        {
+            currentBufferTime -= Time.V_DeltaTime();
+            if (currentBufferTime <= 0f)
+            {
+                bgmState = BGMState.Level1Complete;
+                AudioAPI.PlayBGM(gameObject, "BGM_Vestigal_Perc-Action_End-Hit_Fire-Every_1.714sec_From-Loop-Start_140bpm");
                 currentTransitionTimer = 0f;
                 return;
             }
