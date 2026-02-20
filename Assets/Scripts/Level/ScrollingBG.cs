03293eb2df8058537e5ecd3e930aa6e88a239871
TROOP\elrey <e.wong@digipen.edu>
2026-02-20 14:10:25 +0800

    Added working scrollingbg script on new scene

---
+++ Assets/Scripts/Level/ScrollingBG.cs
@@ -0,0 +1,47 @@
+// Make sure the class name matches the filepath, without space!!.
+// If you want to change class name, change the asset name in the editor!
+// Editor will automatically rename and recompile this file.
+class ScrollingBG : Script
+{
+
+    [SerializableField]
+    private float scrollSpeed = 50f;
+
+    [SerializableField]
+    private float recycleZ = -20f;
+
+    [SerializableField]
+    private float resetZ = 40f;
+
+    // This function is invoked once before init when gameobject is active.
+    protected override void awake()
+    {}
+
+    // This function is invoked once when gameobject is active.
+    protected override void init()
+    {}
+
+    // This function is invoked every update.
+    protected override void update()
+    {
+        Vector3 pos = gameObject.transform.position;
+        pos.z -= scrollSpeed * Time.V_DeltaTime();
+        gameObject.transform.position = pos;
+
+        // Simple teleport if past recycle point
+        if (pos.z < recycleZ)
+        {
+            pos.z = resetZ;
+            gameObject.transform.position = pos;
+        }
+    }
+
+    // This function is invoked every update.
+    protected override void fixedUpdate()
+    {}
+
+    // This function is invoked when destroyed.
+    protected override void exit()
+    {}
+
+}
