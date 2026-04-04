7f8eaf4cb4d98016d8f7772baef5ac5d8450faab
Nyoveee <nyove@hotmail.com>
2026-04-04 17:15:18 +0800

    Bug fixes.

--- Assets/Scripts/VFX/DestroyVFXOnLifetime.cs
+++ Assets/Scripts/VFX/DestroyVFXOnLifetime.cs
@@ -3,6 +3,8 @@
 // Editor will automatically rename and recompile this file.
 class DestroyVFXOnLifetime : Script
 {
+    public float lifetime = 0.5f;
+
     // This function is invoked once before init when gameobject is active.
     protected override void init()
     {
@@ -10,6 +12,6 @@ class DestroyVFXOnLifetime : Script
         Invoke(() =>{
             if (gameObject != null)
                 Destroy(gameObject);
-        }, getComponent<ParticleEmitter_>().lifeTime);
+        }, lifetime);
     }
 }
